b7073663fe2d6039180b4c6c5ebd1d72b1eb7039
Peter Zheng <zhengp@live.co.uk>
2021-05-07 13:51:39 +0100

    Update Flight_Data_Analysis.m

--- Data/Flight_Data_Analysis.m
+++ Data/Flight_Data_Analysis.m
@@ -30,7 +30,7 @@ if ~exist('processed')
     for i=1:n_data
         Data(i).t_raw=Data(i).FD.secs+Data(i).FD.nsecs*1e-9;
         index_t0=find(all(cell2mat(Data(i).FD.Status)=='"Disarmed"',2),1,'first');  % find the index of first "Disarmed"
-        
+        Data(i).delay=str2num(Data(i).name(6:9));
         t=Data(i).IMU.secs+Data(i).IMU.nsecs*1e-9-Data(i).t_raw(index_t0);
         Data(i).ax_imu=interp1(t,Data(i).IMU.x_2,Data(i).t_raw-Data(i).t_raw(index_t0));
         Data(i).ay_imu=interp1(t,Data(i).IMU.y_2,Data(i).t_raw-Data(i).t_raw(index_t0));
@@ -58,6 +58,21 @@ if ~exist('processed')
         Data(i).servo_pos=interp1(t,Data(i).servo_pos,Data(i).t);
         
         %% remove NaN and post perch data
+        while(isnan(Data(i).x(1))||isnan(Data(i).y(1))||isnan(Data(i).z(1)) ...
+                ||isnan(Data(i).vx(1))||isnan(Data(i).vy(1))||isnan(Data(i).vz(1)) ...
+                ||isnan(Data(i).ax_imu(1))||isnan(Data(i).ay_imu(1))||isnan(Data(i).az_imu(1))) % remove NaN
+            Data(i).t(1)=[];
+            Data(i).x(1)=[];
+            Data(i).y(1)=[];
+            Data(i).z(1)=[];
+            Data(i).vx(1)=[];
+            Data(i).vy(1)=[];
+            Data(i).vz(1)=[];
+            Data(i).ax_imu(1)=[];
+            Data(i).ay_imu(1)=[];
+            Data(i).az_imu(1)=[];
+            Data(i).servo_pos(1)=[];
+        end
         while(isnan(Data(i).x(end))||isnan(Data(i).y(end))||isnan(Data(i).z(end)) ...
                 ||isnan(Data(i).vx(end))||isnan(Data(i).vy(end))||isnan(Data(i).vz(end)) ...
                 ||isnan(Data(i).ax_imu(end))||isnan(Data(i).ay_imu(end))||isnan(Data(i).az_imu(end))) % remove NaN
@@ -101,6 +116,8 @@ if ~exist('processed')
         Data(i).ay_imu(1)=[];
         Data(i).az_imu(1)=[];
         Data(i).servo_pos(1)=[];
+        
+        %% remove post release data
         j=length(Data(i).servo_pos);
         while Data(i).servo_pos(j)<max(Data(i).servo_pos)
             Data(i).t(end)=[];
@@ -120,12 +137,12 @@ if ~exist('processed')
             j=j-1;
         end
         %% accel filtering
-        Data(i).ax_filter(1:3)=Data(i).ax(1:3);
-        Data(i).ay_filter(1:3)=Data(i).ay(1:3);
-        Data(i).az_filter(1:3)=Data(i).az(1:3);
-        Data(i).ax_imufilter(1:3)=Data(i).ax_imu(1:3);
-        Data(i).ay_imufilter(1:3)=Data(i).ay_imu(1:3);
-        Data(i).az_imufilter(1:3)=Data(i).az_imu(1:3);
+        Data(i).ax_filter(1:3,1)=Data(i).ax(1:3);
+        Data(i).ay_filter(1:3,1)=Data(i).ay(1:3);
+        Data(i).az_filter(1:3,1)=Data(i).az(1:3);
+        Data(i).ax_imufilter(1:3,1)=Data(i).ax_imu(1:3);
+        Data(i).ay_imufilter(1:3,1)=Data(i).ay_imu(1:3);
+        Data(i).az_imufilter(1:3,1)=Data(i).az_imu(1:3);
         for j=4:length(Data(i).t)
             Data(i).ax_filter(j)=(1+4*Data(i).Dt*wc+2*Data(i).Dt^2*wc^2+Data(i).Dt^3*wc^3)^-1 ...
                 *(Data(i).Dt^3*wc^3*Data(i).ax(j)+(3+10*Data(i).Dt*wc+2*Data(i).Dt^2*wc^2)*Data(i).ax_filter(j-1)-(3+8*Data(i).Dt*wc)*Data(i).ax_filter(j-2)+(1+2*Data(i).Dt*wc)*Data(i).ax_filter(j-3));
@@ -141,11 +158,42 @@ if ~exist('processed')
             Data(i).az_imufilter(j)=(1+4*Data(i).Dt*wc+2*Data(i).Dt^2*wc^2+Data(i).Dt^3*wc^3)^-1 ...
                 *(Data(i).Dt^3*wc^3*Data(i).az_imu(j)+(3+10*Data(i).Dt*wc+2*Data(i).Dt^2*wc^2)*Data(i).az_imufilter(j-1)-(3+8*Data(i).Dt*wc)*Data(i).az_imufilter(j-2)+(1+2*Data(i).Dt*wc)*Data(i).az_imufilter(j-3));
         end
+        
+        %% perch detection
         if Data(i).z(end)>0.4
             Data(i).perch=1;
         else
             Data(i).perch=0;
         end
+        
+        %% isolate data to time during perch
+        index_perch=Data(i).t>=-1 & Data(i).t<=1.5;
+        Data(i).t(~index_perch)=[];
+        Data(i).x(~index_perch)=[];
+        Data(i).y(~index_perch)=[];
+        Data(i).z(~index_perch)=[];
+        Data(i).vx(~index_perch)=[];
+        Data(i).vy(~index_perch)=[];
+        Data(i).vz(~index_perch)=[];
+        Data(i).ax_imu(~index_perch)=[];
+        Data(i).ay_imu(~index_perch)=[];
+        Data(i).az_imu(~index_perch)=[];
+        Data(i).ax(~index_perch)=[];
+        Data(i).ay(~index_perch)=[];
+        Data(i).az(~index_perch)=[];
+        Data(i).ax_imufilter(~index_perch)=[];
+        Data(i).ay_imufilter(~index_perch)=[];
+        Data(i).az_imufilter(~index_perch)=[];
+        Data(i).ax_filter(~index_perch)=[];
+        Data(i).ay_filter(~index_perch)=[];
+        Data(i).az_filter(~index_perch)=[];
+        Data(i).servo_pos(~index_perch)=[];
+        Data(i).jz=zeros(length(Data(i).t),1);
+        
+        %% use max jerk-z as impact
+        Data(i).jz(2:end)=(Data(i).az_filter(2:end)-Data(i).az_filter(1:end-1))/Data(i).Dt;
+        Data(i).index_impact=Data(i).jz==max(Data(i).jz);
+        
         processed=1;
     end
 else
@@ -173,6 +221,7 @@ xlim([-0.5 1.5])
 grid on
 
 figure % 3D xyz
+hold on
 for i=1:n_data
     index_t=Data(i).t>=-1 & Data(i).t<=1.5;
     if Data(i).perch
@@ -180,7 +229,7 @@ for i=1:n_data
     else
         plot3(Data(i).x(index_t),Data(i).y(index_t),Data(i).z(index_t),'--');
     end
-    hold on
+    plot3(Data(i).x(Data(i).index_impact),Data(i).y(Data(i).index_impact),Data(i).z(Data(i).index_impact),'o');
 end
 axis equal
 grid on
@@ -188,15 +237,53 @@ grid on
 figure % xyz at impact
 for i=1:n_data
     if Data(i).perch
-        plot3(Data(i).x(end),Data(i).y(end),Data(i).z(end),'o');
+        plot3(Data(i).x(Data(i).index_impact),Data(i).y(Data(i).index_impact),Data(i).z(Data(i).index_impact),'o');
     else
-%         plot(Data(i).x(index_t),Data(i).y(index_t),'*');
+        plot3(Data(i).x(Data(i).index_impact),Data(i).y(Data(i).index_impact),Data(i).z(Data(i).index_impact),'x');
     end
     hold on
 end
 axis equal
 grid on
-% 
+
+figure % analytics
+plts=5;
+for i=1:n_data
+    subplot(plts,1,1);
+    scatter(Data(i).perch,Data(i).x(Data(i).index_impact));
+    ylabel('x')
+    hold on
+    grid on
+    subplot(plts,1,2);
+    scatter(Data(i).perch,Data(i).vz(Data(i).index_impact));
+    ylabel('vz')
+    hold on
+    grid on
+    subplot(plts,1,3);
+    scatter(Data(i).perch,Data(i).servo_pos(Data(i).index_impact));
+    ylabel('servo pos')
+    hold on
+    grid on
+    subplot(plts,1,4);
+    if Data(i).perch
+        scatter(Data(i).delay,Data(i).z(Data(i).t==0));
+    end
+    xlabel('delay')
+    ylabel('z')
+    hold on
+    grid on
+    subplot(plts,1,5);
+    if Data(i).perch
+        scatter(Data(i).delay,Data(i).servo_pos(Data(i).index_impact));
+    end
+    xlabel('delay')
+    ylabel('servo pos')
+    hold on
+    grid on
+end
+
+
+%
 % figure % need to plot impact velocity
 % for i=1:n_data
 %     if Data(i).perch
